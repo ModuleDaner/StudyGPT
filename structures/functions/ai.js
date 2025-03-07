b179141e892a9645b78fcbf540f1cb9ac0e910a5
Abdalla Ahmed <a.bd0102413@gmail.com>
2025-03-07 18:55:18 +0200

    Update ai.js

--- structures/functions/ai.js
+++ structures/functions/ai.js
@@ -16,7 +16,7 @@ async function Ai(Message) {
     let OldDataArray = [];
     try {
       if (UserSubData.Tier == "1" || UserSubData.Tier == "2") {
-        OldData.slice(UserSubData.Tier === "1" ? -2 : -4).forEach((Item) => {
+        OldData.slice(UserSubData.Tier === "1" ? -1 : -2).forEach((Item) => {
           OldDataArray.push({
             role: "system",
             content: `I have been asked you : ${Item.MyMessage} . And you are give me this response : ${Item.AiMessage}  . Use it as history `,

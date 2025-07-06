f39ba1e94b4d1701522edf4daac47778b9a6548d
Daner <adam.daner1@gmail.com>
2025-07-06 11:30:21 +0300

    Update index.js

--- structures/configuration/index.js
+++ structures/configuration/index.js
@@ -1,10 +1,1 @@
-module.exports = {
-    client_token: "MTMzOTk5Njk1NTA1NjUzNzY4MA.GJAOIK.Kio7blEDYoMFOPOCvpijT2l_BCnZyIlUbVqqjQ",
-    client_id: "1339996955056537680",
-    client_prefix: "!",
-    mongodb_url: "", //optional
-    developers: ["1062197137749852290","760903287343284334"],
-    sharding: true,
-    database: false,
-    dev_guild : "1266510128295710832"
-}
+// this is private
